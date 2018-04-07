629cd6591af3a1f710e29e0511773aa5e68ad2ab
coreymac <coreymac@10.30.5.175>
2018-04-06 22:25:17 -0400

    Began basic implementation of LinkedList, began add method

--- Paranoia/src/Model/CircularLinkedList.java
+++ Paranoia/src/Model/CircularLinkedList.java
@@ -1,5 +1,61 @@
 package Model;
 
-public class CircularLinkedList {
-
+public class CircularLinkedList<Player> {
+	
+	private Node head;
+	private Node tail;
+	
+	public CircularLinkedList() {
+		head = null;
+		tail = null;
+		head.next = tail;
+	}
+	
+	public CircularLinkedList<Player> add(Player element) {
+		Node newNode = new Node(element);
+		Node curr = head;
+		
+		if(head == null) {
+			head = newNode;
+			head.next = tail;
+			tail.next = head;
+			head.prev = tail;
+			tail.prev = head; 
+			tail = head;
+			
+			return this;
+		}
+		
+		if(head.next == null) {
+			head.next = newNode;
+			tail = newNode;
+			tail.prev = head;
+			tail.next = head;
+			head.prev = tail;
+			head.next = tail;
+			
+			return this;
+		}
+		
+		while(curr.next != head){
+			
+		}
+	}
+	
+	
+	
+	public class Node() {
+		
+		protected Player data;
+		protected Node next;
+		protected Node prev;
+		
+		
+		public Node(Player nodeData) {
+			data = nodeData;
+			next = null;
+			prev = null;
+		}
+	}
+	
 }
--- Paranoia/src/Model/Player.java
+++ Paranoia/src/Model/Player.java
@@ -1,5 +1,38 @@
 package Model;
 
 public class Player {
-
+	private String name;
+	private Player target;
+	private Player predator;
+	private int points;
+	
+	
+	public Player(String playerName, Player playerTarget, Player playerPredator) {
+		name = playerName;
+		target = playerTarget;
+		predator = playerPredator;
+		points = 0;
+	}
+	
+	public int getPoints() {
+		return points;
+	}
+	
+	public void targetKilled(Player newTarget) {
+		points++;
+		target = newTarget;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Player getTarget() {
+		return target;
+	}
+	
+	public Player getPredator() {
+		return predator;
+	}
+	
 }

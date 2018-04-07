131fe36a9da53549247c488d7d68033e7b56e84f
coreymac <coreymac@10.30.5.175>
2018-04-06 22:30:42 -0400

    Added setters to Player class, deleted Node class

--- Paranoia/src/Model/CircularLinkedList.java
+++ Paranoia/src/Model/CircularLinkedList.java
@@ -2,13 +2,13 @@ package Model;
 
 public class CircularLinkedList<Player> {
 	
-	private Node head;
-	private Node tail;
+	private Player head;
+	private Player tail;
 	
 	public CircularLinkedList() {
 		head = null;
 		tail = null;
-		head.next = tail;
+		head.getTarget() = tail;
 	}
 	
 	public CircularLinkedList<Player> add(Player element) {
@@ -26,36 +26,8 @@ public class CircularLinkedList<Player> {
 			return this;
 		}
 		
-		if(head.next == null) {
-			head.next = newNode;
-			tail = newNode;
-			tail.prev = head;
-			tail.next = head;
-			head.prev = tail;
-			head.next = tail;
-			
-			return this;
-		}
-		
 		while(curr.next != head){
 			
 		}
 	}
-	
-	
-	
-	public class Node() {
-		
-		protected Player data;
-		protected Node next;
-		protected Node prev;
-		
-		
-		public Node(Player nodeData) {
-			data = nodeData;
-			next = null;
-			prev = null;
-		}
-	}
-	
 }

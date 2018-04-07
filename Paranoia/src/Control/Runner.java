67cd25d64451926cb2e044422b2285b9236f2711
Clark Farley <clarktfarley@gmail.com>
2018-04-07 00:59:07 -0400

    Added to runner class

--- Paranoia/src/Control/Runner.java
+++ Paranoia/src/Control/Runner.java
@@ -7,10 +7,20 @@ import src.Model.CircularLinkedList.Player;
 public class Runner {
 
 	public static void main(String[] args) {
-		CircularLinkedList playerList = new CircularLinkedList();
+		CircularLinkedList<Player> playerList = new CircularLinkedList<Player>();
+		ArrayList<Player> players = new ArrayList<>();
 		
-		Player Clark = new Player
-
+		Player Clark = playerList.new Player("Clark");
+		Player Cory= playerList.new Player("Cory");
+		Player Michael = playerList.new Player("Michael");
+		
+		players.add(Clark);
+		players.add(Cory);
+		players.add(Michael);
+		
+		playerList.addAll(players);
+		
+		playerList.printList();
 	}
 
 }
